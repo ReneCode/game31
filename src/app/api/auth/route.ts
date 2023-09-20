44f56423ff1f177c8d074665965bca66c0d1508d
Reinhard Langner <ReinhardLangner@gmx.de>
2023-09-20 15:27:32 +0200

    random clientId

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -3,7 +3,11 @@ import { NextResponse } from "next/server";
 import Ably from "ably/promises";
 
 export async function GET(request: Request) {
-  const CLIENT_ID = process.env.ABLY_CLIENT_ID || "";
+  const CLIENT_ID =
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15);
+
+  // const CLIENT_ID = process.env.ABLY_CLIENT_ID || "";
   const API_KEY = process.env.ABLY_API_KEY || "";
 
   const client = new Ably.Realtime(API_KEY);
