42eb7cd70f351fc9830930917598187bb6a07d4a
Reinhard Langner <ReinhardLangner@gmx.de>
2023-09-20 14:51:48 +0200

    disable SSR on webSocket Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import styles from "./page.module.css";
 import { AblyProvider } from "ably/react";
 
-import Message from "./components/Message";
+// import Message from "./components/Message";
+
+// https://github.com/ably-labs/react-hooks/issues/8
+// https://ably.com/blog/realtime-chat-app-nextjs-vercel
+
+const Message = dynamic(() => import("./components/Message"), { ssr: false });
 
 const options = { authUrl: "/api/auth" };
 
